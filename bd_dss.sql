5f7683db8a0c52fa793eed3da75458cca7456eb0
Juan <89045210+VReyna7@users.noreply.github.com>
2023-03-28 15:15:53 -0600

    Add files via upload

--- bd_dss.sql
+++ bd_dss.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-03-2023 a las 22:48:16
+-- Tiempo de generación: 28-03-2023 a las 23:15:41
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
