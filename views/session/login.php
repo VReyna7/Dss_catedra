ff564f388986fa40e80dd1f6caee31a4481cbbb4
Juan <89045210+VReyna7@users.noreply.github.com>
2023-03-29 19:39:45 -0600

    Add files via upload

--- views/session/login.php
+++ views/session/login.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/default.css?v=<?php echo time(); ?>">
-    <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/indexStyle.css">
+    <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/indexStyle.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" /><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    <title><?=constant('TITULO')?></title>
 </head>
-<body>
+<body style='background: linear-gradient(rgba(255, 255, 255, 0.178), rgba(255, 255, 255, 0.178)), url("<?php echo constant('URL')?>public/img/fondo1.jpg"'>
     <?php 
         if(isset($_SESSION['USER'])){
             header('location:'.constant('URL').'dashboard/cupones/setCupones');
